390fbff701e8c833f58acd5d710b81f9e655defd
Nakiah Kage-Weir <kiahrae16@outlook.com>
2020-10-20 18:37:48 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+namespace Finch_Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+         Finch myFinch;
+         myfinch = new Finch();
+
+                
+            //
+            // call connect method
+            //
+            myFinch.connect();
+
+            //
+            // Code
+            //
+            finchRobot.setLED(0, 102, 204);
+            finchRobot.wait(3000);
+            finchRobot.setLED(0, 255, 255);
+            finchRobot.wait(3000);
+            finchRobot.setLED(153, 204, 0);
+            finchRobot.wait(3000);
+            finchRobot.noteOff();
+
+            //
+            // disconnect method
+            //
+            myFinch.disconnect();
+        {
+         Console.WriteLine("Press any key to exit");
+                Console.ReadKey();
+        }
+    }
+}
